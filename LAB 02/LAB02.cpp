520db0627ff9fe398c8409a4d5b370bd0ef636b0
lethanhphat1599 <44295537+lethanhphat1599@users.noreply.github.com>
2018-10-19 21:37:21 +0700

    Add files via upload

---
+++ LAB 02/LAB02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+ {
+	cout<<"********  *       *        ***        *********"<<endl;
+	cout<<"*      *  *       *       *   *           *"<<endl;
+	cout<<"*      *  *       *      *     *          *"<<endl;
+	cout<<"********  *********     *********         * "<<endl;
+	cout<<"*         *       *    *         *        *"<<endl;
+	cout<<"*         *       *   *           *       *"<<endl;
+	cout<<"*         *       *  *             *      *";
+	return 0 ;
+	
+}
+
+
